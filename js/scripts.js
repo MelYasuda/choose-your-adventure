31c20552d7434e7d5ac80ab58b56cc174ac9fa3d
Catherine Bradley and Mel Yasuda <mel@>
2018-08-27 11:08:01 -0700

    add commit before pull merge

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,28 @@
-// function Pick() {
-//   if ($("#pickdrunk").click())  {
-//     $("img#drunk").show();
-//   } else if ($("#pickbard").click()) {
-//     $("img#bard").show();
-//   } else if ($("#pickdruid").click()) {
-//     $("img#druid").show();
-//   }
-// }
-
-// function Pick() {
-// var x = document.getElementsByTagName("button")[0].getAttribute("id");
-// console.log(x);
-// }
 
 
-
-// function Pick(clicked_id) {
-//   if(clicked_id === "pickdrunk") {
-//     $("#drunk, #demo").show();
-//     FirstTalk = "drunk";
-//   } else if (clicked_id === "pickbard") {
-//     $("img#bard, #demo").show();
-//     FirstTalk = "bard";
-//   } else if (clicked_id === "pickdruid") {
-//     $("img#druid, #demo").show();
-//     FirstTalk = "druid";
-//   }
-//     console.log(clicked_id);
-//     $("#firstchoice").hide();
-// }
-
-var FirstTalk;
 
 function Pick(clicked_id) {
+  $("#secondchoice").show();
   if(clicked_id === "pickdrunk") {
-    $("#drunk, #demo").show();
-    FirstTalk = "drunk";
+    $("#pickdrunk2, #prompt").show();
   } else if (clicked_id === "pickbard") {
-    $("img#bard, #demo").show();
-    FirstTalk = "bard";
+    $("#pickbard2, #prompt").show();
   } else if (clicked_id === "pickdruid") {
-    $("img#druid, #demo").show();
-    FirstTalk = "druid";
+    $("#pickdruid2, #prompt").show();
   }
     console.log(clicked_id);
     $("#firstchoice").hide();
 }
 
+function drunkTalk(clicked_id) {
+  if(clicked_id === "drunktalk1") {
+    $("#whatdrink").show();
+  } else if (clicked_id === "drunktalk2") {
+    $("#sirdrunk").show();
+  }
+  $("#secondchoice").hide();
+}
 
-
-console.log(FirstTalk);
-
-// function pickagain() {
-//   if(document.getElementById("pickdrunk").innerHTML = true) {
-//     $("img#drunk").show();
-//   } else if (document.getElementById("pickbard").innerHTML = true) {
-//     $("img#bard").show();
-//   }
-//   // return "hello";
-// }
-//
-// function Pick() {
-//  document.getElementById("demo").innerHTML = pickagain();
-//
-// }
-//
-// function Pick() {
-//   var x = document.getElementById("myBtn").getAttribute("onclick");
-//     document.getElementById("demo").innerHTML = x;
-// }
 
 
 
