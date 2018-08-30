44b7e911b0e08eed043c08a1efc4775537898ee4
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-30 09:51:07 -0700

    add finished routes and ends

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 
 function start() {
     $("#intro01").show();
-    $("#firstEnd, #secondEnd, #thirdEnd, #fourthEnd, #fifthEnd, #sixthEnd, #seventhEnd, #eighthEnd, #ninethEnd").hide();
+    $("#introEND01, #dwarfEND01, #dwarfEND02, #dwarfEND03, #dwarfEND04, #dwarfEND05, #dwarfEND06, #dwarfEND07, #dwarfEND08, #dwarfEND09, #lizardEND01, #lizardEND02, #lizardEND03, #lizardEND04, #lizardEND05, #lizardEND06, #fairy01, #fairy02, #fairy03, #fairy04, #fairy05").hide();
 }
 // INTRO
 function intro(clicked_id) {
